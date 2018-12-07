bdfe12904b8f13615abef2d92af21dc8c485b900
Will Dahl <wdahl@albany.edu>
2018-12-06 21:01:54 -0500

    fixed globall varibale issue

--- features_calc.py
+++ features_calc.py
@@ -1,28 +1,27 @@
 import math
 import json
 
-angles = list()
-
 def lenEdge(x1, y1, x2, y2):
     return math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
 
 def angle(x1, y1, x2, y2, x3, y3):
-    global angles
-    angles.append(math.cos(math.degrees(math.acos((math.pow(lenEdge(x1,y1,x2,y2),2) + math.pow(lenEdge(x1,y1,x3,y3),2) - math.pow(lenEdge(x2,y2,x3,y3),2)) / (2*lenEdge(x1,y1,x2,y2)*lenEdge(x1,y1,x3,y3))))))
     return math.cos(math.degrees(math.acos((math.pow(lenEdge(x1,y1,x2,y2),2) + math.pow(lenEdge(x1,y1,x3,y3),2) - math.pow(lenEdge(x2,y2,x3,y3),2)) / (2*lenEdge(x1,y1,x2,y2)*lenEdge(x1,y1,x3,y3)))))
 
 def diffAngles(angle1, angle2):
     return angle2 - angle1
 
 def calcFeatures(graph):
-    global angles
     features = {'V': list(), 'E': list()}
+    angles = list()
     for i in range(len(graph)):
         if len(graph) is 1:
+            angles.append(0)
             V = [0,0,0]
         elif i+1 < len(graph) and i is not 0:
+            angles.append(angle(graph[i][0], graph[i][1], graph[i-1][0], graph[i-1][1], graph[i+1][0], graph[i+1][1]))
             V = [1,angle(graph[i][0], graph[i][1], graph[i-1][0], graph[i-1][1], graph[i+1][0], graph[i+1][1]), math.pow(angle(graph[i][0], graph[i][1], graph[i-1][0], graph[i-1][1], graph[i+1][0], graph[i+1][1]),2)]
         else:
+            angles.append(0)
             V = [1,0,0]
 
         if i>0 and i+1 < len(graph):
