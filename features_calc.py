4bee97cddd75244b27cabd6e71b3f22d0bee6d31
Will Dahl <wdahl@albany.edu>
2018-12-06 19:27:41 -0500

    Now operates on a list of graphs

--- features_calc.py
+++ features_calc.py
@@ -1,27 +1,6 @@
 import math
 import json
 
-features = {'img10': {'V':list(),
-                      'E':list()},
-            'img1':  {'V':list(),
-                      'E':list()},
-            'img2':  {'V':list(),
-                      'E':list()},
-            'img3':  {'V':list(),
-                      'E':list()},
-            'img4':  {'V':list(),
-                      'E':list()},
-            'img5':  {'V':list(),
-                      'E':list()},
-            'img6':  {'V':list(),
-                      'E':list()},
-            'img7':  {'V':list(),
-                      'E':list()},
-            'img8':  {'V':list(),
-                      'E':list()},
-            'img9':  {'V':list(),
-                      'E':list()}}
-
 angles = list()
 
 def dot(x1, y1, x2, y2):
@@ -40,11 +19,12 @@ def angle(x1, y1, x2, y2):
 def diffAngles(angle1, angle2):
     return angle2 - angle1
 
-def calcFeatures(graph, img):
+def calcFeatures(graph):
     global angles
-    global features
     for i in range(len(graph)):
-        if i+1 < len(graph):
+        if len(graph) is 1:
+            V = [0,0,0]
+        elif i+1 < len(graph):
             V = [1,angle(graph[i][0], graph[i][1], graph[i+1][0], graph[i+1][1]), math.pow(angle(graph[i][0], graph[i][1], graph[i+1][0], graph[i+1][1]),2)]
         else:
             V = [1,0,0]
@@ -56,8 +36,10 @@ def calcFeatures(graph, img):
         else:
             E = [0, 0, diffAngles(angles[i-1], angles[i]), math.pow(diffAngles(angles[i-1], angles[i]),2)]
 
-        features[img]['V'].append(V)
-        features[img]['E'].append(E)
-        
-with open('features.json', 'w') as fp:
-    json.dump(features, fp)
+        features['V'].append(V)
+        features['E'].append(E)
+
+
+def getGraph(graphList):
+    for graph in graphList:
+        features = calcFeatures(graph)
