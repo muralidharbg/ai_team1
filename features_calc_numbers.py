69d4ee894d65d19acfc3806b0b54a19da5ba3722
Will Dahl <wdahl@albany.edu>
2018-12-09 10:17:09 -0500

    This code operates on a list of graphs given in the form of Numbers

---
+++ features_calc_numbers.py
@@ -0,0 +1,64 @@
+import math
+import json
+
+def lenEdge(x1, y1, x2, y2):
+    return math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
+
+def angle(x1, y1, x2, y2, x3, y3):
+    return math.cos(math.degrees(math.acos((math.pow(lenEdge(x1,y1,x2,y2),2) + 
+    math.pow(lenEdge(x1,y1,x3,y3),2) - math.pow(lenEdge(x2,y2,x3,y3),2)) / 
+    (2*lenEdge(x1,y1,x2,y2)*lenEdge(x1,y1,x3,y3)))))
+
+def diffAngles(angle1, angle2):
+    return angle2 - angle1
+
+def calcFeatures(graph):
+    features = {'V': list(), 'E': list()}
+    angles = list()
+    for i in range(len(graph)):
+        if len(graph) is 1:
+            angles.append(0)
+            V = [0,0,0]
+        elif i+1 < len(graph) and i is not 0:
+            angles.append(angle(graph[i][0], graph[i][1], graph[i-1][0], 
+            graph[i-1][1], graph[i+1][0], graph[i+1][1]))
+
+            V = [1,angle(graph[i][0], graph[i][1], graph[i-1][0], graph[i-1][1], 
+            graph[i+1][0], graph[i+1][1]), math.pow(angle(graph[i][0], graph[i][1], 
+            graph[i-1][0], graph[i-1][1], graph[i+1][0], graph[i+1][1]),2)]
+        else:
+            angles.append(0)
+            V = [1,0,0]
+
+        if i>0 and i+1 < len(graph):
+            E = [lenEdge(graph[i][0], graph[i][1], graph[i+1][0], graph[i+1][1]), 
+            math.pow(lenEdge(graph[i][0], graph[i][1], graph[i+1][0], graph[i+1][1]),2)
+            , diffAngles(angles[i-1], angles[i]), math.pow(diffAngles(angles[i-1], 
+            angles[i]),2)]
+        elif i+1 < len(graph):
+            E = [lenEdge(graph[i][0], graph[i][1], graph[i+1][0], graph[i+1][1]), 
+            math.pow(lenEdge(graph[i][0], graph[i][1], graph[i+1][0], graph[i+1][1]),2)
+            , 0, 0]
+        else:
+            E = [0, 0, diffAngles(angles[i-1], angles[i]), 
+            math.pow(diffAngles(angles[i-1], angles[i]),2)]
+
+        features['V'].append(V)
+        features['E'].append(E)
+
+    return features
+
+def getGraph(graphList, masterGraph):
+    featureList = list()
+    pointGraph = list()
+    for graph in graphList:
+        for point in graph:
+            pointGraph.append(masterGraph[point])
+
+    featureList.append(calcFeatures(pointGraph))
+
+    return featureList
+
+graphList = [[1,2,3,4,2,5], [4,5,6]]
+
+print(getGraph(graphList))
